4c4497a6244ec3b2028a6778b364a5bed0c92284
NadzeyaValatkevich <nadkonadzya@rambler.ru>
2024-01-31 22:59:17 +0300

    add validate new password

--- src/pages/Info/NewPassword/NewPassword.tsx
+++ src/pages/Info/NewPassword/NewPassword.tsx
@@ -1,12 +1,11 @@
-import { TextFieldComponent } from "@/components/TextFieldComponent";
-import styles from "./NewPassword.module.css";
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
-import { FormContainer, PasswordElement, SubmitHandler } from 'react-hook-form-mui';
+import { FormContainer, PasswordElement, SubmitHandler, PasswordRepeatElement, useForm, useWatch, Controller } from 'react-hook-form-mui';
 import FormGroup from '@mui/material/FormGroup';
-import { useNavigate } from 'react-router-dom'
 import { useAppDispatch } from "@/common/hooks/useAppDispatch";
 import { createNewPassword } from "@/pages/Login/authThunks";
+import { Typography } from '@mui/material'
+
 
 export type FormPasswordValuesType = {
     current_password: string,
@@ -15,57 +14,79 @@ export type FormPasswordValuesType = {
 };
 
 export const NewPassword = () => {
-    const navigate = useNavigate();
     const dispatch = useAppDispatch();
+
+    // const errors: any = {}
+    // if (!values.email) {
+    //     errors.email = 'Required'
+    // } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    //     errors.email = 'Invalid email address'
+    // }
+    // return errors
+
+    const passwordValidation = {
+        validate: (value: string) => {
+            if (! /(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}/g.test(value)) {
+                return 'Пароль не соответствует рекомендациям, указанным в подсказке'
+            }
+            return true
+        },
+    };
 
     const onSuccessHandler: SubmitHandler<FormPasswordValuesType> = async (data: FormPasswordValuesType) => {
         console.log(data)
         await dispatch(createNewPassword(data))
-        navigate('/login')
     }
 
     return (
-        <div className={styles.blockNewPassword}>
-            <Box sx={{ width: "100%" }}>
-                <FormContainer<{ password: string }> onSuccess={onSuccessHandler}>
-                    <FormGroup sx={{ padding: '0 1rem 0 1rem', width: "100%", boxSizing: "border-box" }}>
-                        <PasswordElement
-                            required
-                            helperText={"Введите старый пароль"}
-                            // type={'password'}
-                            margin={'dense'}
-                            name={'current_password'}
-                            variant={'outlined'}
-                            size={"small"}
-                        />
-                        <PasswordElement
-                            required
-                            helperText={"Введите новый пароль"}
-                            type={'password'}
-                            margin={'dense'}
-                            name={'new_password'}
-                            variant={'outlined'}
-                            size={"small"}
-                        />
-                        <PasswordElement
-                            required
-                            helperText={"Подтвердите новый пароль"}
-                            type={'password'}
-                            margin={'dense'}
-                            name={'new_password_confirm'}
-                            variant={'outlined'}
-                            size={"small"}
-                        />
-                        <Button type={'submit'}
-                            variant={'contained'}
-                            color={'primary'}
-                            sx={{ width: "222px", alignSelf: "center", marginTop: "20px" }}>
-                            ИЗМЕНИТЬ ПАРОЛЬ
-                        </Button>
-                    </FormGroup>
+        // <div className={styles.blockNewPassword}>
+        <Box sx={{ width: "100%" }}>
+            <FormContainer<FormPasswordValuesType> onSuccess={onSuccessHandler}>
+                <FormGroup sx={{ padding: '50px 10px 10px 50px', width: "50%", boxSizing: "border-box" }}>
+                    <PasswordElement
+                        required
+                        placeholder={"Введите старый пароль*"}
+                        margin={'dense'}
+                        name={'current_password'}
+                        variant={'outlined'}
+                        size={"small"}
+                        fullWidth={true}
+                    />
+                    <PasswordElement
+                        required
+                        placeholder={"Введите новый пароль*"}
+                        type={'password'}
+                        margin={'dense'}
+                        name={'new_password'}
+                        variant={'outlined'}
+                        size={"small"}
+                        fullWidth={true}
+                        validation={passwordValidation}
+                    />
+                    <Typography> Пароль должен содержать минимум 8 символов: латинские буквы в верхнем и нижнем регистре,
+                        минимум 1 цифра, спецсимволы допускаются.</Typography>
+                    <PasswordRepeatElement
+                        required
+                        placeholder={"Подтвердите новый пароль*"}
+                        passwordFieldName="new_password"
+                        type={'password'}
+                        margin={'dense'}
+                        name={'new_password_confirm'}
+                        variant={'outlined'}
+                        size={"small"}
+                        fullWidth={true}
+                        customInvalidFieldMessage={"Пароль должен совпадать"}
+                    />
+                    <Button type={'submit'}
+                        variant={'contained'}
+                        color={'primary'}
+                        sx={{ width: "222px", alignSelf: "center", marginTop: "20px" }}>
+                        ИЗМЕНИТЬ ПАРОЛЬ
+                    </Button>
+                </FormGroup>
 
-                </FormContainer>
-            </Box>
-        </div>
+            </FormContainer>
+        </Box>
+        // </div>
     )
 }
